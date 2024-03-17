d09398e81eb9b21cf6f76d15edb4b83624bee7eb
Wendy Paez <paezwendy32@gmail.com>
2024-03-16 20:22:40 -0500

    feat(q2): solve memory and time

--- src/q2_memory.py
+++ src/q2_memory.py
@@ -1,4 +1,21 @@
+import pandas as pd
+import emoji
 from typing import List, Tuple
+from utils import load_json_as_df
+
+def extract_emojis(text: str) -> List[str]:
+    return [c for c in text if c in emoji.EMOJI_DATA]
 
 def q2_memory(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    df = load_json_as_df(file_path, include_content=True)
+    emojis_count = {}
+    for text in df['content']:
+        emojis = extract_emojis(text)
+        for emoji in emojis:
+            emojis_count[emoji] = emojis_count.get(emoji, 0) + 1
+    top_emojis = sorted(emojis_count.items(), key=lambda x: x[1], reverse=True)[:10]
+    return top_emojis
+
+if __name__ == "__main__":
+    result = q2_memory('./input/farmers-protest-tweets-2021-2-4.json')
+    print(result)
